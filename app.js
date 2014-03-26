d5872efdd9c6ec9c8906d49b1ee6e8338f0bdb77
gthetwong <gthetwong@gmail.com>
2014-03-26 16:34:35 -0700

    updated JSON endpoints to match new attributes being sent by pi. will now try to push to heroku and add data in new format

--- app.js
+++ app.js
@@ -28,50 +28,19 @@ if ('development' == app.get('env')) {
 }
 
 
-// var params = { host: 'ec2-107-22-163-140.compute-1.amazonaws.com',user: 'yjluvfzfeipgtt',password: 'H6c46uR71OrqAytEjwV5FtaMwY',database: 'd9ln0vulna1mlo', ssl: true };
-
+//set up our client connection info for Postgres
 var client = new pg.Client(process.env.DATABASE_URL);
 
+//Create Necessary Tables in Postgres
 var createTable = function(){
   client.connect();
   client.query('CREATE TABLE IF NOT EXISTS soildata (id SERIAL PRIMARY KEY, reading INTEGER, pi_id INTEGER, sensor_id INTEGER, isdry Boolean)');
   client.query('CREATE TABLE IF NOT EXISTS piunits (id SERIAL PRIMARY KEY, ownedby TEXT, serial_num INTEGER, redline INTEGER)');
-  // client.query('INSERT INTO soildata(reading, user_id, plant_id, redline, isdry) VALUES(750, 1, 1, 800, false)');
-  // client.query('INSERT INTO soildata(reading, user_id, plant_id, redline, isdry) VALUES(600, 1, 2, 1000, false)');
-  // client.query('INSERT INTO soildata(reading, user_id, plant_id, redline, isdry) VALUES(900, 1, 1, 800, true)');
 }; 
 
 
-app.get('/', function(req, res){
-  res.render('index.html');
-});
-
-
-app.post('/:plant_id/:user_id/:reading', function(req, res){
-  
-  var data = req.params; 
-  console.log(data);
-
-  var plant_id = data.plant_id;
-  var user_id = data.user_id;
-  var reading = data.reading;
-  console.log(plant_id);
-  console.log(user_id);
-  console.log(reading);
-
-// client.query('INSERT INTO soildata(reading, user_id, plant_id, redline, isdry) VALUES('+reading+', '+user_id+', '+plant_id+', 800, false)');
-//   console.log("inserted into database");
-//   res.send(req.params);
-});
-
-createTable();
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-
-app.get('/api', function(req, res){ 
+//get all soil data
+app.get('/data', function(req, res){ 
   query = client.query('SELECT * FROM soildata', function(err, result){
     if(!result){
       return res.send('no data');
@@ -81,16 +50,68 @@ app.get('/api', function(req, res){
   });
 });
 
-
-app.get('/api/:user_id', function(req, res){
-  var id = req.params.user_id;
-  console.log(id);
-  query = client.query('SELECT * FROM soildata WHERE user_id = ' + id, function(err, result){
+//get all pi units
+app.get('/pi', function(req, res){
+  query = client.query('SELECT * FROM piunits',function(err, result){
     if(!result){
       return res.send('no data');
     } else {
       res.send(result);
     }
-  });
+  }); 
+});
+
+//Add sensor data to given plant under given pi
+app.post('/:reading/:pi_id/:sensor_id', function(req, res){
+  
+  var data = req.params; 
+  console.log(data);
+
+  var reading = data.reading;
+  var pi_id = data.pi_id;
+  var sensor_id = data.sensor_id;
+  console.log(plant_id);
+  console.log(user_id);
+  console.log(reading);
+
+client.query('INSERT INTO soildata(reading, pi_id, sensor_id) VALUES('+reading+', '+pi_id+', '+sensor_id+')');
+  res.send(req.params);
+});
+
+
+//add User's new Pi Unit to Pi Unit database
+app.post('/register/:ownedby/:serial_num/:redline', function(req, res){
+  
+  var data = req.params; 
+  console.log(data);
+
+  var ownedby = data.ownedby;
+  var serial_num = data.serial_num;
+  var redline = data.redline;
+  console.log(ownedby);
+  console.log(serial_num);
+  console.log(redline);
+
+client.query('INSERT INTO piunits(ownedby, serial_num, redline) VALUES('+ownedby+', '+serial_num+', '+redline+')');
+  res.send(req.params);
+});
+
+//Get soil data based on user_id  <<-------------------(Deprecated)
+// app.get('/api/:user_id', function(req, res){
+//   var id = req.params.user_id;
+//   console.log(id);
+//   query = client.query('SELECT * FROM soildata WHERE user_id = ' + id, function(err, result){
+//     if(!result){
+//       return res.send('no data');
+//     } else {
+//       res.send(result);
+//     }
+//   });
+
+createTable();
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 });
