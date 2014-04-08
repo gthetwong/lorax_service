b88113cad1b6feeb4fec9d191938f8c0a5b03846
jongmini <jongmin.choi@gmail.com>
2014-04-07 17:34:19 -0700

    console logging with comments

--- app.js
+++ app.js
@@ -92,11 +92,11 @@ app.post('/:reading/:pi_id/:sensor_id', function(req, res){
           console.log("reading is moist");
           dryness = false;
         }
-        console.log(dryness);
+        console.log(dryness,"first");
         return dryness;
       },
         function(dryness){
-        console.log(dryness);
+        console.log(dryness,"second");
         client.query('INSERT INTO soildata(reading, pi_id, sensor_id, recordtime, isdry) VALUES($1, $2, $3, $4, $5)', [reading, pi_id, sensor_id, date, dryness],
             function(err, result){
                 if (err){console.log(err, "error inserting to PG");}
