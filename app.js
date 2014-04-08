b175ec746b822dfb48fa830e5cf9ff79f4294024
gthetwong <gthetwong@gmail.com>
2014-04-07 18:34:19 -0700

    removed parent function

--- app.js
+++ app.js
@@ -78,7 +78,8 @@ app.post('/:reading/:pi_id/:sensor_id', function(req, res){
   console.log(reading);
   console.log(date);
 
-  function(){ client.query('SELECT redline FROM piunits INNER JOIN soildata ON pi_id = serial_num WHERE soildata.sensor_id = piunits.sensor_id Limit 1',
+ 
+    client.query('SELECT redline FROM piunits INNER JOIN soildata ON pi_id = serial_num WHERE soildata.sensor_id = piunits.sensor_id Limit 1',
     function(err, result){
       console.log((result.rows[0].redline), "this is the result");
       var redline_value = result.rows[0].redline; 
@@ -102,7 +103,7 @@ app.post('/:reading/:pi_id/:sensor_id', function(req, res){
                 res.send(req.params);
             });
     });
-  }
+  
 
 
 
