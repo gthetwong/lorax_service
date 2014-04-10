f1e96e0dd2f69d47bd98a8d5358b65af0def3b23
gthetwong <gthetwong@gmail.com>
2014-04-10 14:58:10 -0700

    console logging result rows

--- app.js
+++ app.js
@@ -96,16 +96,16 @@ app.post('/:reading/:pi_id/:sensor_id', function(req, res){
       if(dryness){
         client.query('SELECT * FROM soildata WHERE pi_id = '+ pi_id +' AND sensor_id = '+sensor_id + 'AND isdry = true ORDER BY recordtime desc limit 1', function(err, result){
           if (err){console.log(err);}
-          console.log((date - result.rows[0].recordtime));
+          console.log(result.rows);
       // request.post("http://projectlorax.herokuapp.com/notify/" + ownedby+"/"+pi_id+"/"+sensor_id);
         });
       }
 
-        client.query('INSERT INTO soildata(reading, pi_id, sensor_id, recordtime, isdry) VALUES($1, $2, $3, $4, $5)', [reading, pi_id, sensor_id, date, dryness],
-            function(err, result){
-                if (err){console.log(err, "error inserting to PG");}
-                res.send(req.params);
-        });
+        // client.query('INSERT INTO soildata(reading, pi_id, sensor_id, recordtime, isdry) VALUES($1, $2, $3, $4, $5)', [reading, pi_id, sensor_id, date, dryness],
+        //     function(err, result){
+        //         if (err){console.log(err, "error inserting to PG");}
+        //         res.send(req.params);
+        // });
     });
 });
 
