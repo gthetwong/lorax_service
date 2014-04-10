3eb20cb29b773d4a77638dc68261920dc1f10b87
gthetwong <gthetwong@gmail.com>
2014-04-10 15:54:45 -0700

    successfully converted date to js datetime. trying to subtract datetimes and console log the value

--- app.js
+++ app.js
@@ -97,8 +97,10 @@ app.post('/:reading/:pi_id/:sensor_id', function(req, res){
         client.query('SELECT * FROM soildata WHERE pi_id = \''+ pi_id +'\' AND sensor_id = '+sensor_id + 'AND isdry = true ORDER BY recordtime desc limit 1', function(err, result){
           if (err){console.log(err);}
           var last_tweet = new Date(result.rows[0].recordtime);
+          var currentdate = new Date(date);
           console.log(last_tweet);
-          console.log(date);
+          console.log(currentdate);
+          console.log(currentdate-last_tweet);
           res.send("200, success");
       // request.post("http://projectlorax.herokuapp.com/notify/" + ownedby+"/"+pi_id+"/"+sensor_id);
         });
